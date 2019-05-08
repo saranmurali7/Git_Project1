5d3d5c26563ef54399ef9d40e951999725268ecd
saran.muraleedharan@i-exceed.com <saran.muraleedharan@i-exceed.com@IXED-LAP0263>
2019-05-08 17:29:09 +0530

    Second Commit

--- Git_Project1/src/com/package1/NewTest.java
+++ Git_Project1/src/com/package1/NewTest.java
@@ -1,6 +1,8 @@
 package com.package1;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
@@ -16,7 +18,8 @@ public class NewTest
 	  System.setProperty(key, value);
 	  driver=new FirefoxDriver();
 	  driver.get(url);
-	  
+      WebElement home=driver.findElement(By.xpath("/html/body/div[2]/section[2]/div/div/div/div/div/div/div[1]/div[2]/ul/li[2]/a/span"));
+	  home.click();
 	  
   }
 }
